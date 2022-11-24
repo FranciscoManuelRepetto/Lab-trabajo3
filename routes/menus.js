358bf46fc2c50af39811cebf77abe79b823b56df
Jerexio <jeremiasrodriguez05@gmail.com>
2022-11-24 16:48:47 -0300

    Finalizando api menus

--- routes/menus.js
+++ routes/menus.js
@@ -3,35 +3,50 @@ const routerMenu = express.Router();
 
 const menus = require('../database/db_menus');
 
-const logger = require('./login')
-
 const dayjs = require('dayjs');
-const path = require('path');
-const request = require('request');
-
-routerMenu.get('/:date', (req, res) => {
-    let todayMenu = getMenuDate(dayjs(req.body.date));
-    if(todayMenu.length === 0){
-        res.status(404).send('Error 404: Not Found');
-    }
-    res.status(200).send(todayMenu);
-});
 
 routerMenu.get('/', (req, res) => {
     res.json(menus);
 });
 
-routerMenu.post('/', (req, res) => {
-    let dateReq = getDayjsFormat(req.body.fecha);
-    let menuObtained = menus.find(menu => getDayjsFormat(menu.fecha).isSame(dateReq));
-    if(menuObtained === null){
-        menus.push(req.body);
-        return res.status(200).json(menus);
+routerMenu.get('/:date', (req, res) => {
+    let todayMenu = getMenuDate(dayjs(req.params.date));
+    if(todayMenu.length === 0){
+        res.status(404).json({Error: 'Not Found'});
     }
-    res.status(503);
+    res.status(200).send(todayMenu);
 });
 
-//FALTA PUT, PATCH Y DELETE
+routerMenu.get('/:dateInit/:dateEnd', (req, res) => {
+    let date1 = dayjs(req.params.dateInit);
+    let date2 = dayjs(req.params.dateEnd);
+    if(date1 === null && date2 === null){
+        res.status(400).json({Error: 'Date format is invalid, use YYYY-MM-DD'});
+    }
+
+    if(date1.isAfter(date2)){
+        res.status(400).json({Error: 'Date initial is after date end'});
+    }
+
+    let menus = getMenusBetween(date1, date2);
+
+    if(menus.length === 0){
+        res.status(406).json({Error: 'Menus between dates was not found'});
+        
+    }
+    res.status(200).send(menus);
+});
+
+routerMenu.post('/', (req, res) => {
+    let dateReq = getDayjsFormat(req.body.fecha);
+    let menuObtained = menus.find(menu => getDayjsFormat(menu.fecha).isSame(dateReq)) || null;
+    console.log(menuObtained);
+    if(menuObtained !== null){
+        res.status(409).json({Error:'Already menu with date loaded'});
+    }
+    menus.push(req.body);
+    res.status(201).json(menus);
+});
 
 const getDayjsFormat = (dateJSON) => {
     return dayjs(`${dateJSON.year}-${dateJSON.month}-${dateJSON.day}`);
@@ -41,6 +56,14 @@ const getMenuDate = (date) => {
     const dateMenus = menus.find(menu => {
         let dateLook = getDayjsFormat(menu.fecha);
         return dateLook.isSame(date, 'day');
+    });
+    return dateMenus;
+};
+
+const getMenusBetween = (date1, date2) => {
+    const dateMenus = menus.findAll(menu => {
+        let dateLook = getDayjsFormat(menu.fecha);
+        return dateLook.isBetween(date1, date2, 'day');
     });
     return dateMenus;
 };
